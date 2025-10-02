500b23c96aa9bdd2fe0344c56977ff649d8ad693
Lounis04 <Lounisdany@gmail.com>
2025-10-02 22:38:21 +0200

    premier-affichage-couleurs-retournement-etc

--- cartes.py
+++ cartes.py
@@ -2,12 +2,23 @@ import os
 
 class Cartes():
     "La couleur de la carte correspond à si c'est un trèfle,un coeur,etc.. et sa valeur correspond à si c'est 1,4,roi,etc..."
-    def __init__(self,nom,couleur,valeur , image = None):
-        self.retour = True
+    def __init__(self,nom,famille,valeur,couleur,image = None):
+        self.retour = False
         self.nom = nom
-        self.couleur = couleur
+        self.famille = famille
         self.valeur = valeur
+        self.couleur = couleur
         self.image = image
+
+    def retournement_cartes(self):
+        "Méthode qui permet de retourner une carte"
+        self.retour = True
+
+    def affichage_retournement_cartes(self):
+        "Méthode qui permet de changer l'image d'une carte à une carte retournée si elle doit etre retournée"
+        if self.retour == True:
+            self.image = os.path.join(dossier_cartes,"carte_retour.png")
+
 
 #Liste qui contient toutes les cartes 
 
@@ -16,19 +27,33 @@ jeu = []
 #Liste des valeurs,couleurs,noms possibles#
 
 valeurs = ["1","2","3","4","5","6","7","8","9","10","11","12","13"]
-couleurs = ["coeur","carreau","trefle","pique"]
+familles = ["coeur","carreau","trefle","pique"]
 noms = ["As","2","3","4","5","6","7","8","9","10","Valet","Dame","Roi"]
+couleurs = ["Rouge","Noir"]
 
 #Initialisation des 52 cartes#
 dossier_cartes = "cartes"
 
-for couleur in couleurs:
+for famille in familles:
     for valeur, nom in zip(valeurs, noms): #On parallélise les 2 listes qui sont de meme longueur vu que la valeur à le même indice que le nom dans les deux listes#
-        chemin_image = os.path.join(dossier_cartes, f"{valeur}_{couleur}.gif")
-        carte = Cartes(nom=nom, couleur=couleur, valeur=int(valeur), image=chemin_image)
+        if famille == familles[0] or famille == familles[1]:
+            couleur = couleurs[0]
+        else:
+            couleur = couleurs[1]
+        chemin_image = os.path.join(dossier_cartes, f"{valeur}_{famille}.gif")
+        carte = Cartes(nom=nom, famille = famille, valeur=int(valeur),couleur = couleur, image=chemin_image)
         jeu.append(carte)
 
 #Test des cartes#
 
+
 #for c in jeu:
-    #print(c.nom, c.couleur, c.valeur, c.image)
+    #print(c.nom, c.famille, c.valeur,c.couleur, c.image)
+
+#c = jeu[0]  # la première carte du jeu
+#print("Avant retournement :", c.nom, c.famille, c.valeur, c.couleur, c.image)
+
+#c.retournement_cartes()
+#c.affichage_retournement_cartes()
+
+#print("Après retournement :", c.nom, c.famille, c.valeur, c.couleur, c.image)
--- main.py
+++ main.py
@@ -1,28 +1,69 @@
 import tkinter as tk
+import random
+from cartes import jeu
 
-visuel = tk.Tk()
-visuel.title("Solitaire")
-visuel.configure(bg="green")
+def lancement():
+    visuel = tk.Tk()
+    visuel.title("Solitaire")
+    visuel.configure(bg="green")
 
-#Prise d'info sur la taille de l'écran de l'utulisateur#
+    global largeur_ecran #x
+    global longueur_ecran #y
+    largeur_ecran = visuel.winfo_screenwidth()
+    longueur_ecran = visuel.winfo_screenheight()
 
-largeur_ecran = visuel.winfo_screenwidth()
-longueur_ecran = visuel.winfo_screenheight()
+    espace_cartes = tk.Canvas(visuel, width =  largeur_ecran, height = longueur_ecran, bg="green", highlightthickness=0)
+    espace_cartes.pack()
 
-#Création de l'instance espace_cartes qui correspond aux rectangles en pointillés blancs#
+    espace_cartes.create_rectangle(largeur_ecran * 0.01 ,longueur_ecran * 0.01, largeur_ecran * 0.1, longueur_ecran * 0.22, outline="white", dash=(5, 3), width=2)
+    espace_cartes.create_rectangle(largeur_ecran * 0.01 ,longueur_ecran * 0.23, largeur_ecran * 0.1, longueur_ecran * 0.45, outline="white", dash=(5, 3), width=2)
+    espace_cartes.create_rectangle(largeur_ecran * 0.01 ,longueur_ecran * 0.46, largeur_ecran * 0.1, longueur_ecran * 0.68, outline="white", dash=(5, 3), width=2)
+    espace_cartes.create_rectangle(largeur_ecran * 0.01 ,longueur_ecran * 0.69, largeur_ecran * 0.1, longueur_ecran * 0.91, outline="white", dash=(5, 3), width=2)
 
-espace_cartes = tk.Canvas(visuel, width =  largeur_ecran, height = longueur_ecran, bg="green", highlightthickness=0)
-espace_cartes.pack()
+    espace_cartes.create_rectangle(largeur_ecran*0.9, longueur_ecran*0.23,largeur_ecran*0.99, longueur_ecran*0.45,outline="white", dash=(5,3), width=2)
 
-#Définition des coordonnées des rectangles#
+    x_btn = largeur_ecran * 0.945   
+    y_btn1 = longueur_ecran * 0.5  
+    y_btn2 = longueur_ecran * 0.55
+    y_btn3 = longueur_ecran * 0.6
 
-espace_cartes.create_rectangle(largeur_ecran * 0.01 ,longueur_ecran * 0.01, largeur_ecran * 0.1, longueur_ecran * 0.22, outline="white", dash=(5, 3), width=2)
-espace_cartes.create_rectangle(largeur_ecran * 0.01 ,longueur_ecran * 0.23, largeur_ecran * 0.1, longueur_ecran * 0.45, outline="white", dash=(5, 3), width=2)
-espace_cartes.create_rectangle(largeur_ecran * 0.01 ,longueur_ecran * 0.46, largeur_ecran * 0.1, longueur_ecran * 0.68, outline="white", dash=(5, 3), width=2)
-espace_cartes.create_rectangle(largeur_ecran * 0.01 ,longueur_ecran * 0.69, largeur_ecran * 0.1, longueur_ecran * 0.91, outline="white", dash=(5, 3), width=2)
+    btn_perdu = tk.Button(visuel, text="S'avouer perdu", command=visuel.destroy, bg="red", fg="white")
+    btn_perdu.place(x=x_btn, y=y_btn1, anchor="n")
 
-espace_cartes.create_rectangle(largeur_ecran*0.9, longueur_ecran*0.23,largeur_ecran*0.99, longueur_ecran*0.45,outline="white", dash=(5,3), width=2)
+    btn_piocher = tk.Button(visuel, text="Piocher 3 cartes", bg="yellow")
+    btn_piocher.place(x=x_btn, y=y_btn2, anchor="n")
 
-#Lancement du programme#
+    btn_piocher = tk.Button(visuel, text="Relancer la pioche", bg="yellow")
+    btn_piocher.place(x=x_btn, y=y_btn3, anchor="n")
 
-visuel.mainloop()
+    random.shuffle(jeu)
+
+    images = mise_en_place(espace_cartes, jeu, largeur_ecran, longueur_ecran)
+
+    visuel.mainloop()
+
+def mise_en_place(espace_cartes, jeu, largeur_ecran, longueur_ecran):
+    """Place les 7 colonnes du Solitaire sur le canvas."""
+    images = [] 
+    x_depart = largeur_ecran * 0.15
+    y_depart = longueur_ecran * 0.01
+    decalage_x = largeur_ecran * 0.1
+    decalage_y = longueur_ecran * 0.05   
+    index = 0  
+    for col in range(7):
+        for ligne in range(col + 1):
+            carte = jeu[index]
+            index += 1
+            if ligne < col:
+                carte.retournement_cartes()
+                carte.affichage_retournement_cartes()
+            img = tk.PhotoImage(file=carte.image) #Charge l'image en question#
+            images.append(img)  
+            x = x_depart + col * decalage_x
+            y = y_depart + ligne * decalage_y
+            espace_cartes.create_image(x, y, image=img, anchor="nw")
+    return images
+
+lancement()
+
+
